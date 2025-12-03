d1558365de92961df1a96c492737d505b6f06793
Santiago Nunez <santillanix@users.noreply.github.com>
2025-12-03 17:05:20 +0100

    global franchise selection

--- src/app/components/dimension-info/dimension-info.ts
+++ src/app/components/dimension-info/dimension-info.ts
@@ -1,6 +1,10 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, inject, computed } from '@angular/core';
 import { dimensionType } from '../../features/dimensions/dimensions.model';
 import { CommonModule } from '@angular/common';
+import { FranchiseService } from '../../features/dimensions/franchise/franchise.service';
+import { CollectionService } from '../../features/dimensions/collection/collection.service';
+import { SubCollectionService } from '../../features/dimensions/subcollection/subcollection.service';
+import { ManufacturerService } from '../../features/dimensions/manufacturer/manufacturer.service';
 
 @Component({
   selector: 'app-dimension-info',
@@ -9,6 +13,37 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dimension-info.scss',
 })
 export class DimensionInfo {
+  // Inject all dimension services
+  private franchiseService = inject(FranchiseService);
+  private collectionService = inject(CollectionService);
+  private subcollectionService = inject(SubCollectionService);
+  private manufacturerService = inject(ManufacturerService);
+
+  // Input: dimension type
   type = input.required<dimensionType>();
-  total = input<number>(0);
+
+  // Computed: total count based on dimension type
+  total = computed(() => {
+    const dimensionType = this.type();
+
+    switch (dimensionType) {
+      case 'franchise':
+        return this.franchiseService.franchiseCount();
+      case 'collection':
+        return this.collectionService.collectionCount();
+      case 'subcollection':
+        return this.subcollectionService.subcollectionCount();
+      case 'manufacturer':
+        return this.manufacturerService.manufacturerCount();
+      default:
+        return 0;
+    }
+  });
+
+  ngOnInit() {
+    this.franchiseService.ensureFranchisesLoaded();
+    this.collectionService.ensureCollectionsLoaded();
+    this.subcollectionService.ensureSubCollectionsLoaded();
+    this.manufacturerService.ensureManufacturersLoaded();
+  }
 }
