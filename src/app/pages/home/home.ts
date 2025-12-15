61a7b6b7b5e37907dfab7ed327c2c4a2bee652ab
Santiago Nunez <santillanix@users.noreply.github.com>
2025-12-15 18:42:39 +0100

    refactor: Simplify product form by removing unused dropdown components and image selector

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { TitlePage } from "../../components/title-page/title-page";
-import { FormPimageSelector } from '../../components/form/form';
 
 @Component({
   selector: 'app-home',
-  imports: [TitlePage, FormPimageSelector],
+  imports: [TitlePage],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home implements OnInit {
-
-  selectedImages: string[] = [];
 
   ngOnInit() {
 
